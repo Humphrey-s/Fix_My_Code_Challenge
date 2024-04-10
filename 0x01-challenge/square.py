b5a420c45c59c8dd493ce2ff24e45985c5df8218
greybeard <greybeard@gmail.com>
2024-04-10 12:18:51 +0300

    2 commit

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+class Square definition
+"""
 
-class square():
-    
+class Square():
+   """
+   Computes square properties
+   """
     width = 0
     height = 0
 
@@ -14,7 +19,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def Permiter_of_my_square(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -22,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
